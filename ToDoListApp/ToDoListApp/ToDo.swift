a722c34e72ef61e198ca892126dac1e526e468b8
ziahellman <64755146+ziahellman@users.noreply.github.com>
2020-05-03 19:28:09 -0400

    Add files via upload
    Iteration 0

---
+++ ToDoListApp/ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Zia Hellman on 5/3/20.
+//  Copyright © 2020 Zia Hellman. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+}
---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Zia Hellman on 5/3/20.
+//  Copyright © 2020 Zia Hellman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
